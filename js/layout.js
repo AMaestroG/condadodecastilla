272889386ffa3edbfa0171025c6fdebb177fa8eb
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-11-23 23:20:29 -0300

    Fix: Relative paths for local viewing and responsiveness check

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/_header.html')
+    fetch('_header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
             initializeSidebarNavigation(); 
         });
 
-    fetch('/_footer.html')
+    fetch('_footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
